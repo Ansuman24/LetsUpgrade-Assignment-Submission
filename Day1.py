2533d4533831c896480257a9b404f6112013edf2
ANSUMAN DAS <78476265+Ansuman24@users.noreply.github.com>
2021-08-17 09:51:07 +0530

    Day 1 - Assignment Submitted

---
+++ Day1.py
@@ -0,0 +1,8 @@
+                # ASSIGNMENT 1
+# Q - Take 2 inputs and store in variable x & y.Find x to the power y                 
+
+
+x=int(input("enter 1st num: "))
+y=int(input("enter 2nd num: "))
+r=x**y
+print(r)
