0bfdf4eb31d56e3c1e6dd54be2482021c69a5a8c
ANSUMAN DAS <78476265+Ansuman24@users.noreply.github.com>
2021-08-22 20:07:29 +0530

    Assignment 3 Submitted

---
+++ DAY3 Project.py
@@ -0,0 +1,9 @@
+                  #MINI PROJECT:Generating Random 6 character OTP to user
+
+import random
+import string
+otp = ''
+characters_list=list(string.ascii_letters+string.digits)
+for i in range(6):
+    otp += random.choice(characters_list)
+print(otp)    
